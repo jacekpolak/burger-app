ad3d56d4c7a7057d617bdcf1ee813bd6f4971b8a
Jacek Polak <jacek.polak@xfive.co>
2018-01-24 23:33:56 +0100

    Add modal backdrop, change styles, new buttons on modal

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,9 +2,7 @@ import React from 'react';
 import css from './Backdrop.css';
 
 const backdrop = (props) => (
-  <div className={css.Backdrop}>
-
-  </div>
+  props.show ? <div className={css.Backdrop} onClick={props.close}></div> : null
 );
 
 export default backdrop;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import css from './Modal.css';
+import Hoc from '../../../hoc/hoc';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
-  <div 
-    className={css.Modal}
-    style={{
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1': '0',
-    }} >
-    {props.children}
-  </div>
+  <Hoc>
+    <Backdrop show={props.show} close={props.close} />
+    <div 
+      className={css.Modal}
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1': '0',
+      }} >
+      {props.children}
+    </div>
+  </Hoc>
 );
 
 export default modal;
