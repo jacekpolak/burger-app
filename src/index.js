26b516567083a853fe62dd17143a18eb00987c09
Jacek_Polak <jacek_polak@epam.com>
2018-03-14 12:28:14 +0100

    Middleware for redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './store/reducer';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching ', action);
+      const result = next(action);
+      console.log('[Middleware] next state ', store.getState());
+      return result;
+    }
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
+// const store = createStore(reducer, applyMiddleware(logger));
 
 const app = (
   <Provider store={store}>
