89df61e9ae25c98480df876ad21154a70b8acf06
Jacek_Polak <jacek_polak@epam.com>
2018-03-16 17:26:06 +0100

    Linear reducers

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -18,30 +18,35 @@ const INGREDIENT_PRICES = {
   bacon: 0.8,
 };
 
+const addIngredient = (state, action) => {
+  return updateObject(state,
+    {
+      ingredients: {
+        ...state.ingredients,
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+      },
+      totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+  );
+};
+
+const removeIngredient = (state, action) => {
+  return updateObject(state,
+    {
+      ingredients: {
+        ...state.ingredients,
+        [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+      },
+      totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+    }
+  );
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_INGREDIENT:
-      return updateObject(state,
-        {
-          ingredients: {
-            ...state.ingredients,
-            [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-          },
-          totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-        }
-      );
-    case actionTypes.REMOVE_INGREDIENT:
-      return updateObject(state,
-        {
-          ingredients: {
-            ...state.ingredients,
-            [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-          },
-          totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-        }
-      );
-    default:
-      return state
+    case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);
+    case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
+    default: return state
   }
 }
 
