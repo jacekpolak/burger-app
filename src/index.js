ec3a09229224020f1db4c33da430be2b14a0aa3f
Jacek_Polak <jacek_polak@epam.com>
2018-03-16 17:19:50 +0100

    Add utility for object Update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import reducer from './store/reducer';
+import reducer from './store/reducers/reducer';
 
 const logger = store => {
   return next => {
